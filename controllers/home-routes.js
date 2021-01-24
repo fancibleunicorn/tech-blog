976a041eb5a9bdf2ca4901ab5785223c11241052
Adam Crandall <adamcrandall91@gmail.com>
2021-01-23 23:11:54 -0500

    styled single post page and added button filters

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User} = require('../models');
+const { Post, User, Comment} = require('../models');
 
 router.get('/', (req, res) => {
   console.log(req.session);
@@ -20,7 +20,10 @@ router.get('/', (req, res) => {
       })
         .then(dbPostData => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('homepage', { posts });
+            res.render('homepage', { 
+              posts,
+              loggedIn: req.session.loggedIn
+             });
         })
         .catch(err => {
           console.log(err);
@@ -35,6 +38,52 @@ router.get('/login', (req, res) => {
   }
   
   res.render('login');
+});
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_text',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-post', { post,
+      loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
