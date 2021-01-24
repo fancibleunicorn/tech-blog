439aebbebce1815e2a69c74806c1da7ecbd7046f
Adam Crandall <adamcrandall91@gmail.com>
2021-01-24 18:18:39 -0500

    set expire for 5 minutes

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
